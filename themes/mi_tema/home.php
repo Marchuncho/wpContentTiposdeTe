a977681327ed42d6510ad90fd3f87e37f761abb5
Marco <marxunxo@gmail.com>
2019-10-15 14:07:33 -0300

    tipos de te wp content

---
+++ themes/mi_tema/home.php
@@ -0,0 +1,114 @@
+<?php get_header() ?>
+
+<section id="tea" class="pb-5">
+  <div class="container">
+    <h1 class="section-title h1">Tipos de té</h1>
+    <div class="row">
+      <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+          <div class="mainflip">
+            <div class="frontside">
+              <div class="card">
+                <div class="card-body text-center">
+                  <p><img class=" img-fluid" src="<?php echo get_theme_file_uri(); ?>/assets/images/te_blanco.png" alt="card image"></p>
+                  <h4 class="card-title">Té Blanco</h4>
+                  <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="backside">
+              <div class="card">
+                <div class="card-body text-center mt-4">
+                  <h4 class="card-title">Té Blanco</h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+          <div class="mainflip">
+            <div class="frontside">
+              <div class="card">
+                <div class="card-body text-center">
+                  <p><img class=" img-fluid" src="<?php echo get_theme_file_uri(); ?>/assets/images/te_verde.png" alt="card image"></p>
+                  <h4 class="card-title">Té Verde</h4>
+                  <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="backside">
+              <div class="card">
+                <div class="card-body text-center mt-4">
+                  <h4 class="card-title">Té Verde</h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+          <div class="mainflip">
+            <div class="frontside">
+              <div class="card">
+                <div class="card-body text-center">
+                  <p><img class=" img-fluid" src="<?php echo get_theme_file_uri(); ?>/assets/images/te_rojo.png" alt="card image"></p>
+                  <h4 class="card-title">Té Rojo</h4>
+                  <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="backside">
+              <div class="card">
+                <div class="card-body text-center mt-4">
+                  <h4 class="card-title">Té Rojo</h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+          <div class="mainflip">
+            <div class="frontside">
+              <div class="card">
+                <div class="card-body text-center">
+                  <p><img class=" img-fluid" src="<?php echo get_theme_file_uri(); ?>/assets/images/te_negro.png" alt="card image"></p>
+                  <h4 class="card-title">Té Negro</h4>
+                  <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="backside">
+              <div class="card">
+                <div class="card-body text-center mt-4">
+                  <h4 class="card-title">Té Negro</h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="more_info text-center">
+      <h2>La planta de té</h2>
+      <p>El té es la infusión preparada con las hojas secas molidas o brotes del arbusto Camellia sinensis en agua caliente.</p>
+
+      <p>El arbusto tiene muchas ramificaciones, de entre 1 y 2 metros de altura, y puede alcanzar, aunque muy raramente, incluso los 12 metros. Las hojas son siempre verdes, lanceoladas hacia adelante e incluso del revés u oblongo-ovadas, de unos 4 cm de ancho y entre 3 y 12 cm de largo, sobre las que destaca una fuerte nerviación. Las flores están en grupos de dos o tres o bien aisladas, de color blanco, inclinadas hacia abajo, y son aromáticas. Las flores de Camellia japonica son erguidas.</p>
+
+      <p>El arbusto ha crecido silvestre a lo largo de la historia en Extremo Oriente, aunque actualmente se cultiva en muchos otros lugares, incluso sobre terreno poco fértil utilizando terrazas.</p>
+    </div>
+  </div>
+</section>
+<?php get_footer() ?>
